47ad116dafaf88dd207ea039f172cc51ed3578c8
admin <admin@admindeMacBook-Pro.local>
2020-05-12 21:03:07 +0800

    flask 上下文与出入栈

--- app/test/test.py
+++ app/test/test.py
@@ -7,5 +7,13 @@ from flask import Flask, current_app, request, Request
 app = Flask(__name__)
 
 
+# 应用上下文  对象  Flask 封装
+# 请求上下文  对象  Request 封装
+# Flask AppContext
+# Request RequestContext
+# 离线应用、单元测试
+ctx = app.app_context()
+ctx.push()
 a = current_app
 d = current_app.config['DEBUG']
+ctx.pop()
