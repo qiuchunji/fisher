8148be6408c5d5fb7914cad30acc300f503412ab
admin <admin@SHANB008121mac.local>
2020-05-13 14:48:29 +0800

    详解上下文管理器__exit__

--- app/test/test.py
+++ app/test/test.py
@@ -24,8 +24,12 @@ app = Flask(__name__)
 1. 连接数据
 2. sql
 3. 释放资源
-__enter__
-__exit__
+__enter__  
+    连接数据库
+with :  (上下文表达式)
+    操作数据库的
+__exit__ 
+    释放资源
 """
 
 # 文件的读写
@@ -39,9 +43,9 @@ __exit__
 #     print(f.read())
 
 
-with app.app_context():
-    a = current_app
-    d = current_app.config['DEBUG']
+# with app.app_context():
+#     a = current_app
+#     d = current_app.config['DEBUG']
 
 
 # 实现了上下文协议的对象使用with
@@ -49,13 +53,26 @@ with app.app_context():
 # __enter__  __exit__
 # 上下文表达式必须返回一个上下文管理器
 
-# class A:
-#     def __enter__(self):
-#         a = 1
-#
-#     def __exit__(self):
-#         b = 2
-#
-#
-# with A() as obj_A:
-#     pass
+class MyResource:
+    def __enter__(self):
+        print('connect to resource')
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        if tb:
+            print('process exception')
+        else:
+            print('no exception')
+        print('close resource connection')
+        return False  # True 不抛出异常, 去掉return 即返回False
+
+    def query(self):
+        print('query data')
+
+
+try:
+    with MyResource() as resource:
+        1/0
+        resource.query()
+except Exception as ex:
+    pass
